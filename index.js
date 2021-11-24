dc2d52d850c1a2baa377c0acb76385adc7a21cb3
Jessica Nelson <yahwehschica@yahoo.com>
2021-11-24 12:07:51 -0500

    commit tree f5f0b522bd6dbcc4794b3aa83fb61a201563651c

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
-/* Your Code Here */
+const createEmployeeRecord = employeeRecord => {
+    return {
+        firstName: employeeRecord[0],
+        familyName: employeeRecord[1],
+        title:employeeRecord[2],
+        payPerHour: employeeRecord[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }   
+}
+    
+const createEmployeeRecords = empRecords => {
+    return empRecords.map(rec => createEmployeeRecord(rec))
+}  
+
+const createTimeInEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(" ") 
+    const clockIn = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(clockIn)
+    return this 
+    
+}
+
+const createTimeOutEvent = function(dateStamp) {
+    const[date, hour] = dateStamp.split(" ")
+    const clockOut = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(clockOut)
+    return this 
+}
+
+const hoursWorkedOnDate = function(formDate) {
+    const cameIn = this.timeInEvents.find(timeIn => 
+        timeIn.date === formDate)
+    const wentOut = this.timeOutEvents.find(timeOut =>
+        timeOut.date === formDate)
+
+    return (wentOut.hour - cameIn.hour) / 100
+}
+
+const wagesEarnedOnDate = function(formDate) {
+    return hoursWorkedOnDate.call(this, formDate) * this.payPerHour    
+}
+
+const findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(records => records.firstName === firstName) 
+}
+
+const calculatePayroll = function(recordsArray) {
+    return recordsArray.reduce((total, record) => {
+        return total + allWagesFor.call(record)
+    }, 0)
+}
+
+
+
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
